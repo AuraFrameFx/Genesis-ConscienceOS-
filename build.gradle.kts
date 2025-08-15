3e1a88ee6b58a6129f9dc550baa0c2ca2f4fad44
AuraKai <wehttam1989@gmail.com>
2025-08-15 14:31:33 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@
 // August 15, 2025 - BLEEDING EDGE VERSION CATALOG COMPLIANT
 
 plugins {
-    id("com.android.application") version "8.11.1" apply false
-    id("com.android.library") version "8.11.1" apply false
+    id("com.android.application") version "9.1.0-rc1" apply false
+    id("com.android.library") version "9.1.0-rc1" apply false
     id("org.jetbrains.kotlin.android") version "2.2.20-Beta2" apply false
     id("org.jetbrains.kotlin.jvm") version "2.2.20-Beta2" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.20-Beta2" apply false
